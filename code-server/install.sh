6f328bd39891c0fc46d0c8b815a1e4f2f9d25463
caoyufei <atcaoyufei@gmail.com>
2023-03-05 15:30:19 +0800

    code server

--- code-server/install.sh
+++ code-server/install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-apt-get update && apt-get install --no-install-recommends ca-certificates zsh git vim wget curl net-tools -y 
+apt-get update && apt-get install --no-install-recommends ca-certificates openssh-client zsh git vim wget curl net-tools -y 
 
 ARCH=$(dpkg --print-architecture)
 
@@ -15,6 +15,7 @@ CODE_RELEASE='4.10.1'
 
 #CODE_RELEASE=$(curl -sX GET https://api.github.com/repos/coder/code-server/releases/latest | awk '/tag_name/{print $4;exit}' FS='[""]' | sed 's|^v||')
 echo "${ARCH}-${CODE_RELEASE}"
+cd /tmp
 
 curl -o /tmp/code-server.tar.gz -L "https://github.com/coder/code-server/releases/download/v${CODE_RELEASE}/code-server-${CODE_RELEASE}-linux-${ARCH}.tar.gz"
 tar zxf /tmp/code-server.tar.gz -C /usr/local/ 
@@ -32,19 +33,21 @@ echo 'alias ll=ls -la' >> ~/.zshrc
 echo 'alias vim=vim' >> ~/.zshrc
 echo 'cd /data/code-server/workspace' >> ~/.zshrc
 
-# # nvm node
-# curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | zsh
-# nvm install 16
-# node --version
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git
+echo "source ${(q-)PWD}/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> ${ZDOTDIR:-$HOME}/.zshrc
 
-# # python
-# curl https://pyenv.run | zsh
-# echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.zshrc
-# echo 'command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.zshrc
-# echo 'eval "$(pyenv init -)"' >> ~/.zshrc
+# nvm node
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | zsh
+nvm install 16
+node --version
 
+# python
+curl https://pyenv.run | zsh
+echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.zshrc
+echo 'command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.zshrc
+echo 'eval "$(pyenv init -)"' >> ~/.zshrc
 
-mkdir -p /data/code-server/{extensions,data,workspace}  
+mkdir -p /data/code-server/{extensions,data,workspace}
 # chown -R coder:coder /data/code-server 
 apt clean
 rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/* 
