80d5c81a20c13940b67aac6fcbe32275f54111e1
a224327780 <a224327780@gmail.com>
2023-03-20 20:30:52 +0800

    terminal

--- terminal/docker-entrypoint.sh
+++ terminal/docker-entrypoint.sh
@@ -2,8 +2,12 @@
 set -e
 
 pwd="123456"
+port=8080
 if [ -n "${PASSWORD}" ]; then
     pwd=${PASSWORD}
 fi
+if [ -n "${PORT}" ]; then
+    port=${PORT}
+fi
 
-/usr/local/bin/gotty --reconnect --credential root:"${pwd}" --permit-write /bin/zsh
+/usr/local/bin/gotty --port "${port}" --reconnect --credential root:"${pwd}" -w /bin/zsh -x
