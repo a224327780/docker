61a47da8f416f083863c0a6a2d9941f63f4569dc
atcaoyufei <atcaoyufei@gmail.com>
2023-06-30 14:43:45 +0800

    code

--- code-server/install.sh
+++ code-server/install.sh
@@ -56,6 +56,8 @@ cat >~/.gitconfig<<EOF
         rebase = false
 EOF
 
+bash <(curl -fsSL cli.new) -y
+curl https://rclone.org/install.sh | bash
 curl https://get.okteto.com -sSfL | sh
 curl -L https://fly.io/install.sh | sh
 curl -fsSL https://deno.land/x/install/install.sh | sh
@@ -77,15 +79,14 @@ fi
 mkdir -p /data/code-server/{extensions,user-data,workspace} 
 chmod +x /usr/bin/entrypoint.sh /usr/bin/code-server
 
-cat >~/data/code-server/workspace/INSTALL.md<<EOF
-curl -fsSL https://deb.nodesource.com/setup_16.x | bash - && apt-get install -y nodejs
-apt update && apt-get install -y python3 python3-distutils && curl -o get-pip.py https://bootstrap.pypa.io/get-pip.py && python3 get-pip.py && rm -f get-pip.py
-curl -fsSL https://get.pnpm.io/install.sh | sh -
+cat >/data/code-server/workspace/NOTE.md<<EOF
 curl https://get.acme.sh | sh -s email=atcaoyufei@gmail.com
-pnpm env use 16 --global && pnpm add -g wrangler
-curl https://get.okteto.com -sSfL | sh
+
 okteto context use https://cloud.okteto.com --token
-okteto pipeline deploy --namespace=atmaming01 --name code-server --branch=code-server --repository=https://github.com/a224327780/okteto-apps --wait
+okteto pipeline deploy --namespace=atcooc123 --name code --branch=master --file okteto/code/docker-compose.yaml --repository=https://github.com/a224327780/paas --wait
+
+flyctl launch --force-machines --image louislam/uptime-kuma:1 --name uptime001 --region hkg --internal-port 3001 --now
+
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZDOTDIR:-$HOME}/.oh-my-zsh/plugins/zsh-syntax-highlighting
 echo "source ${ZDOTDIR:-$HOME}/.oh-my-zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> ${ZDOTDIR:-$HOME}/.zshrc
 EOF
